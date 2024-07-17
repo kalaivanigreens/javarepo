e71acfcee33ae672c98fd22d6d145181a98fbd4f
kalaivanigreens <kalaikarikalan04@gmail.com>
2024-07-17 14:48:51 +0530

    this is my first project

---
+++ src/org/newgit/SampleGit.java
@@ -0,0 +1,23 @@
+package org.newgit;
+
+public class SampleGit {
+	public static void main(String[] args) {
+		
+	
+	int a=20;
+	if(a%2==0) {
+		System.out.println("a is Even");
+	}
+	else {
+		System.out.println("a is odd");
+	}
+	
+	
+	
+	
+	
+	}
+	
+	
+
+}
